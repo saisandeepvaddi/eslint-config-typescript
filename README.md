0c013832037206df422424ad99c4cff19fbd6b30
Sai <saisandeepvaddi@gmail.com>
2021-01-16 16:29:24 -0500

    add support to node

--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 
 My eslint config for typescript-based projects.
 
-
 ```shell
-yarn add -D eslint @saisandeepvaddi/eslint-config-typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react prettier
+yarn add -D eslint @saisandeepvaddi/eslint-config-typescript @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier eslint-plugin-react prettier eslint-plugin-node eslint-plugin-jsx-a11y
 ```
 
 ```js
@@ -13,5 +12,4 @@ yarn add -D eslint @saisandeepvaddi/eslint-config-typescript @typescript-eslint/
 module.exports = {
   extends: "@saisandeepvaddi/eslint-config-typescript",
 };
-
 ```
--- index.js
+++ index.js
@@ -14,10 +14,13 @@ module.exports = {
     },
   },
   extends: [
+    "eslint:recommended",
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:jsx-a11y/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:node/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -31,7 +34,6 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
-
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["warn"],
   },
