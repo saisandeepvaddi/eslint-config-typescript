26a97f6da01abcccf8062dfb51ce8e42e9f8754c
Sai <saisandeepvaddi@gmail.com>
2021-01-18 22:41:56 -0500

    add prettier config

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ module.exports = {
     "react-app", // Uses the recommended rules from @eslint-plugin-react
     "plugin:jsx-a11y/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -36,9 +36,20 @@ module.exports = {
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/interface-name-prefix": "off",
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["warn"],
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuotes: false,
+        semi: true,
+        trailingComma: "all",
+        printWidth: 80,
+        tabWidth: 2,
+      },
+    ],
   },
   globals: {
     React: "writable",
