aacb059f49a9f40bdab55a319406d55e3b235aee
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2021-06-12 14:38:59 -0700

    update prettier styles

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier"],
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
@@ -22,8 +22,7 @@ module.exports = {
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "prettier",
   ],
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -48,6 +47,7 @@ module.exports = {
         trailingComma: "es5",
         printWidth: 80,
         tabWidth: 2,
+        endOfLine: "auto",
       },
     ],
   },
